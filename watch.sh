4feadf879fd44efc6f3994b3e3d051eb547b5372
Adam J. Butler <85082674+adam248@users.noreply.github.com>
2024-01-29 03:12:54 +0000

    + custom python file watcher script for hot running tests

---
+++ watch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+		echo "Usage: $0 <python_file>"
+		exit 1
+fi
+
+stars="*********************************************************"
+python_file=$1
+
+while true; do
+		# Use inotifywait to monitor file changes
+
+		# Specific event
+		change=$(inotifywait --event access,move_self "$python_file" 2>/dev/null)
+
+		# All events
+		#change=$(inotifywait "$python_file" 2>/dev/null)
+
+		if [ $? -eq 0 ]; then
+				# File changed, execute it
+				echo " "
+				echo "$stars"
+				echo "$(date): File changed, running $python_file"
+				python "$python_file"
+				echo "$stars"
+				echo " "
+		fi
+done
